b6bee61abd07a88fcf58c544f57a6686fc59d690
bilunabeel <bilunabeelmv@gmail.com>
2023-04-13 16:44:24 +0530

    Updates

---
+++ precache-manifest.c4dabce10ba98d130c3492bcf9a9cc61.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bfc3b38dc7c5f9f4dfc",
+    "url": "/bilunabeelmv/static/css/main.470eb8b5.chunk.css"
+  },
+  {
+    "revision": "7bfc3b38dc7c5f9f4dfc",
+    "url": "/bilunabeelmv/static/js/main.7bfc3b38.chunk.js"
+  },
+  {
+    "revision": "624c0433ecd2f20f8d62",
+    "url": "/bilunabeelmv/static/css/1.c0231353.chunk.css"
+  },
+  {
+    "revision": "624c0433ecd2f20f8d62",
+    "url": "/bilunabeelmv/static/js/1.624c0433.chunk.js"
+  },
+  {
+    "revision": "f80ae66ff175a9b24359",
+    "url": "/bilunabeelmv/static/js/runtime~main.f80ae66f.js"
+  },
+  {
+    "revision": "8d278ea3fddd6336d27842e3e44d51cf",
+    "url": "/bilunabeelmv/static/media/cv.8d278ea3.pdf"
+  },
+  {
+    "revision": "0a8cf10ed941810c0051a81e9a1572eb",
+    "url": "/bilunabeelmv/static/media/me.0a8cf10e.png"
+  },
+  {
+    "revision": "7d1fe24b3072b92a2bd11f8a5d569f9b",
+    "url": "/bilunabeelmv/static/media/me-about.7d1fe24b.jpg"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/bilunabeelmv/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/bilunabeelmv/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/bilunabeelmv/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/bilunabeelmv/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/bilunabeelmv/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/bilunabeelmv/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "4b326a292b493311e05af2b7b4df9f2e",
+    "url": "/bilunabeelmv/index.html"
+  }
+];
